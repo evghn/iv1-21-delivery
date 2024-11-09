96cc0dd92e68fb0f5fff50309db4919933f40220
evghn <evghn94_git@mail.ru>
2024-11-10 02:49:58 +0300

    auto change settings in config db

--- config/db.php
+++ config/db.php
@@ -1,14 +1,17 @@
 <?php
 
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;port=33061;dbname=delivery_21',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
-];
+return $_SERVER["DOCUMENT_ROOT"] == 'E:/OSPanel/domains/iv1-21-delivery'
+    ? [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=localhost;port=3306;dbname=delivery_21',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8',        
+    ]
+    : [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=localhost;port=33061;dbname=delivery_21',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8',        
+    ];
