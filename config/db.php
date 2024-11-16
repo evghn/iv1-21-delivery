d6a2cebd943c791552fe4cfe6a04822778b0e156
evghn <evghn94_git@mail.ru>
2024-11-17 00:12:58 +0300

    set config osPanel v.6

--- config/db.php
+++ config/db.php
@@ -1,17 +1,31 @@
 <?php
+// var_dump($_SERVER); die;
 
-return $_SERVER["DOCUMENT_ROOT"] == 'E:/OSPanel/domains/iv1-21-delivery'
-    ? [
+if ($_SERVER["DOCUMENT_ROOT"] == 'E:/OSPanel/domains/iv1-21-delivery') {
+    return [
         'class' => 'yii\db\Connection',
         'dsn' => 'mysql:host=localhost;port=3306;dbname=delivery_21',
         'username' => 'root',
         'password' => '',
         'charset' => 'utf8',        
-    ]
-    : [
+    ];
+    
+}
+
+if (isset($_SERVER["SERVER_ADDR"]) && $_SERVER["SERVER_ADDR"] != '127.0.0.1') {
+     return [
         'class' => 'yii\db\Connection',
-        'dsn' => 'mysql:host=localhost;port=33061;dbname=delivery_21',
+        'dsn' => 'mysql:host=MySQL-8.2;port=3306;dbname=delivery_21',
         'username' => 'root',
         'password' => '',
         'charset' => 'utf8',        
     ];
+}
+
+return  [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;port=33061;dbname=delivery_21',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',        
+];
