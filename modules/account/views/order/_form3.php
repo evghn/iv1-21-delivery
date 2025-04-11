8b6e6c36cf56fbafe57b678ab1261e247d3b8217
evghn <evghn94_git@mail.ru>
2025-04-11 11:40:55 +0300

    добавление alert при создании заказа

--- modules/account/views/order/_form3.php
+++ modules/account/views/order/_form3.php
@@ -26,11 +26,17 @@ use yii\widgets\Pjax;
             'id' => 'form-order',           
         ]); ?>
   
-            <?= $form->field($model, 'date')->textInput(['type' => 'date']) ?>
+            <?= $form->field($model, 'date')->textInput(['type' => 'date', 'min' => date('Y-m-d', strtotime('+1 day'))]) ?>
+
+            <?= $form->field($model, 'date')->textInput(['type' => 'date', 'min' => date('Y-m-d', time() + 3600*24)]) ?>
 
             <?= $form->field($model, 'time')->textInput(['type' => 'time']) ?>
+            <?= $form->field($model, 'year')->widget(\yii\widgets\MaskedInput::class, [
+    'mask' => '9999',
+]) ?>
 
             <?= $form->field($model, 'type_pay_id')->dropDownList(TypePay::getTypePay(), ['prompt' => 'Выберете способ оплаты']) ?>
+            <div class="alert alert-primary alert-pay d-none" role="alert">Необходимо наличе устройства с возможностью сканирования QR кода</div>
 
             <?= $form->field($model, 'address')->textInput(['maxlength' => true]) ?>
 
@@ -38,7 +44,9 @@ use yii\widgets\Pjax;
 
             <?= $form->field($model, 'check')->checkbox() ?>
 
-            <?= $form->field($model, 'comment')->textInput(['maxlength' => true, 'disabled' => ! (bool)$model->check]) ?>
+            <?= $model->check ? $form->field($model, 'comment')->textInput(['maxlength' => true]) : '' ?>
+            
+            <?php #  $form->field($model, 'comment')->textInput(['maxlength' => true, 'disabled' => ! (bool)$model->check]) ?>
 
             <div class="form-group">
                 <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
--- web/js/order3.js
+++ web/js/order3.js
@@ -13,4 +13,14 @@ $(() => {
     $('#order-pjax').on('pjax:complete', function() {
         $('#form-order').submit();
     })
+
+    $('#order-pjax').on('change', '#order-type_pay_id',  function() {
+        // if ($('#order-type_pay_id option:selected').text() == "По QR-коду") {
+        // if ($('#order-type_pay_id option:selected').val() == "3") {
+        if ($(this).val() == "3") {
+            $(".alert-pay").removeClass("d-none")
+        } else {
+            $(".alert-pay").addClass("d-none")
+        }
+    })
 })
